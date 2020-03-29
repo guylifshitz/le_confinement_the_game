668ec4988e609a2874c8bc3868a30a3e7437b1f2
Guy <guylifshitz@gmail.com>
2020-03-29 13:28:07 +0200

    - cleanup win screen

--- scripts/global.gd
+++ scripts/global.gd
@@ -2,6 +2,6 @@ extends Node
 
 var items_recovered = []
 var bonus_items_recovered = []
-var score
+var score = -1
 var play_audio = "all"
 
--- scripts/win-screen.gd
+++ scripts/win-screen.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
 func _ready():
-	$score/text.bbcode_text = "[right]"+str(int(global.score))
+	$dialog/bonus_score/text.bbcode_text = str(int(global.score))
 
 	for bonus_item in global.bonus_items_recovered:
-		$bonus_items.get_node(bonus_item).show()
+		$dialog/bonus_items.get_node(bonus_item).show()
 	if global.bonus_items_recovered.size() == 0:
-		$bonus_items/none.show()
+		$dialog/bonus_items/none.show()
 
 func next_scene():
 	get_tree().change_scene("res://main-menu.tscn")
